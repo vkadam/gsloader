2ab3fa21dd5f96fe337a0f1c2b70f289d9f76861
Vishal Kadam <vishal.4kadam@gmail.com>
2012-10-16 18:07:09 -0400

    grunt.js cleanup

--- grunt.js
+++ grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: '<json:gsloader.json>',
         meta: {
-            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
         },
         concat: {
             dist: {
@@ -41,7 +41,6 @@ module.exports = function(grunt) {
                 newcap: true,
                 noarg: true,
                 sub: true,
-                //undef: true,
                 boss: true,
                 eqnull: true,
                 browser: true,
@@ -57,9 +56,8 @@ module.exports = function(grunt) {
         uglify: {}
     });
 
-    // Register tasks.
     grunt.loadNpmTasks("grunt-jasmine-runner");
 
-    // Default task.
+    // Register tasks.
     grunt.registerTask("default", "jasmine lint concat min");
 };
