0e4dec348c48d5d2ec2eca2466d2b12cb1567faa
Vishal Kadam <vishal.4kadam@gmail.com>
2012-10-17 16:31:33 -0400

    Resolved conflict

--- grunt.js
+++ grunt.js
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-/*global module:false*/
-module.exports = function(grunt) {
-
-    // Project configuration.
-    "use strict";
-    grunt.initConfig({
-        pkg: '<json:gsloader.json>',
-        meta: {
-            banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-        },
-        concat: {
-            dist: {
-                separator: ';\n/**********************************/\n',
-                src: ['<banner:meta.banner>', '<file_strip_banner:src/js/gsloader.js>', '<file_strip_banner:src/js/plugins/gsloader-drive.js>', '<file_strip_banner:src/js/plugins/gsloader-auth.js>'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
-        min: {
-            dist: {
-                src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-                dest: 'dist/<%= pkg.name %>.min.js'
-            }
-        },
-        lint: {
-            files: ['grunt.js', 'src/js/**/*.js']
-        },
-        jasmine: {
-            src: ['src/lib/**/*.js', 'src/js/**/*.js'],
-            specs: ['jasmine/lib/**/*.js', 'jasmine/specs/**/*Spec.js']
-        },
-        watch: {
-            files: '<config:lint.files>',
-            tasks: 'lint'
-        },
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                //unused: true,
-                debug: true
-            },
-            globals: {
-                jQuery: false,
-                console: true,
-                gapi: true
-            }
-        },
-        uglify: {}
-    });
-
-    grunt.loadNpmTasks("grunt-jasmine-runner");
-
-    // Register tasks.
-    grunt.registerTask("default", "jasmine lint concat min");
-=======
 /*global module:false*/
 module.exports = function(grunt) {
 
@@ -128,5 +64,4 @@ module.exports = function(grunt) {
     // Register tasks.
     grunt.registerTask("default", "jasmine clean lint concat min");
     grunt.registerTask("test", "jasmine clean");
->>>>>>> Fixed grunt issues. Cleared _SpecRunner.html
 };
